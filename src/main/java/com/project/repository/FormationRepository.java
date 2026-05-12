3eb451ae6c13bacd24a7aa2284ceea0d8be0e2da
sarrastambouli <sarahstambouli8@gmail.com>
2026-05-12 22:09:01 +0100

    fix hichem

--- src/main/java/com/project/repository/FormationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import com.project.entity.Formation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormationRepository extends JpaRepository<Formation, Long> {
-}
